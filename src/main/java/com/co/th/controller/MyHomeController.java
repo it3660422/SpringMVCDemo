0c4d7539e8be8c8a848bd22e03c73e5b6850457d
kuntongh <kun_turtle@hotmail.com>
2018-07-16 19:55:37 +0700

    add lib

--- src/main/java/com/co/th/controller/MyHomeController.java
+++ src/main/java/com/co/th/controller/MyHomeController.java
@@ -1,10 +1,19 @@
 package com.co.th.controller;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import Model.DbCon;
+import Model.UsersDto;
 
 @Controller
 public class MyHomeController {
@@ -14,6 +23,23 @@ public class MyHomeController {
 		mav.setViewName("my-home");
 		mav.addObject("title", "xx");
 		mav.addObject("msg", "Testss");
+		Connection con = new DbCon().con();
+		Statement stmt;
+		try {
+			stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from users");
+			ArrayList<UsersDto> lst = new ArrayList<UsersDto>();
+			UsersDto a = new UsersDto();
+			while (rs.next()) {
+				a = new UsersDto();
+				a.setName(rs.getString("name"));
+				lst.add(a);
+			}
+			mav.addObject("title", lst.get(0).getName());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	 return mav;
  }
  @RequestMapping(value = "/{name:.+}", method = RequestMethod.GET)
